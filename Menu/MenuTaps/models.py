460474a72a2706dc60813022692871c41ac66ebe
FiifiQwontwo <quophi555@gmail.com>
2023-03-04 22:30:15 +0000

    ModelCategory

--- Menu/MenuTaps/models.py
+++ Menu/MenuTaps/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
