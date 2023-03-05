dff77d194e90913915db648228841b96dcd83f59
FiifiQwontwo <quophi555@gmail.com>
2023-03-05 20:06:59 +0000

    ModelCategory

--- Menu/MenuTaps/admin.py
+++ Menu/MenuTaps/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, SubCategory
+
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name',)
+    list_display_links = ('category_name',)
+    # am ordering  date joined by descending order
+    ordering = ('-created_at',)
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('subcategory', 'category_name',)
+
+
+admin.site.register(SubCategoryAdmin, Category, SubCategory, SubCategoryAdmin)
--- Menu/MenuTaps/models.py
+++ Menu/MenuTaps/models.py
@@ -1,3 +1,5 @@
+from unicodedata import category
+
 from django.db import models
 
 
@@ -12,3 +14,20 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
+
+
+class SubCategory(models.Model):
+    subcategory_name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'subcategory'
+        verbose_name_plural = 'subcategories'
+
+    def __str__(self):
+        return self.subcategory_name
